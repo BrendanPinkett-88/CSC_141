ce23533de0a08ec14ba92b7c325c8ead56b2f616
Dreaded-Ghost& C:\Users\bpink\AppData\Local\Python\pythoncore-3.14-64\python.exe c:/Users/bpink/OneDrive/Desktop/CSC_141/test.py <brendan.pinkett001@albright.edu>
2026-09-03 12:38:39 -0400

    new commit

---
+++ 02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,5 @@
+# I like cookies and cream ice cream because of the cookies inside
+print("I like cookies and cream because of the cookies inside")
+
+# The thing is my favorite character because he is strong and can take a lot of damage
+print("My favorite character is strong and can take a lot of damage")
---
+++ 02_variables_and_data_types/02_12_zen_of_python.py
@@ -0,0 +1,2 @@
+import this
+print("I like watching marvel movies because of the characters and the action scenes and the comedy.")
